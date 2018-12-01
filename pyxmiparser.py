ebeacc11e67c9be9595a5b751b5ced11bbad2ab1
binaryflesh <logan.campos123@gmail.com>
2018-12-01 15:22:48 -0600

    Basic structure added.

---
+++ pyxmiparser.py
@@ -0,0 +1,3 @@
+# This is meant to serve as the application file duct taping everything together
+#
+# Copyright (C) 2018 Logan Campos - @binaryflesh
---
+++ sbin/xmi_db.py
@@ -0,0 +1,4 @@
+# This file is meant to serve as database layer and management of sqlite3 to store XMI spec variables
+#
+# Copyright (C) 2018 Logan Campos - @binaryflesh
+#
